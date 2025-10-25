290f022d3e796118d5fb22715ff3e4dc9949a2f0
maxpryimak-yeg <maxpryimak22@gmail.com>
2025-10-25 15:05:47 -0600

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import localFont from "next/font/local";
+import Script from "next/script";
 import "./globals.css";
 import Footer from './components/Footer';
 import FooterContact from './components/FooterContact';
@@ -38,6 +39,23 @@ const ClientLayout = ({ children }) => {
   return (
     <html lang="en" className={gloriousSans.className}>
       <body className="antialiased bodyBg">
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-0V3VT30J5C"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-0V3VT30J5C');
+            `,
+          }}
+        />
         <PageAnimatePresence>
           <div className="relative">
             {children}
